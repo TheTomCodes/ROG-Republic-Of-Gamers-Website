6eeebeadb39e712047bef77c3a73d289730d197e
Aglodiya MahammadAli Sabirhusain <119345786+TheTomCodes@users.noreply.github.com>
2023-10-17 21:45:18 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "**/*.{html,js}",
+     "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
